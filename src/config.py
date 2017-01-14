41c56f29a55bee02b21ce1e39134577fad014aa3
Jon Peterson <jonathandpeterson@yahoo.com>
2017-01-14 10:45:05 -0800

    Added WSGI file

--- src/config.py
+++ src/config.py
@@ -11,10 +11,7 @@ IP_ADDRESS = '0.0.0.0'
 PORT=5000
 SECRET_KEY = 'rF&UM39t6Rn2S6422776H9e3!*5D62*K'
 
-# Defining the database
-DATABASE_NAME = 'books.db'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, DATABASE_NAME)
-DATABASE_CONNECT_OPTIONS = {}
+
 
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
